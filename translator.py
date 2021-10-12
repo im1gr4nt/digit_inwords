7b7795028c72a4caccb22297ef93d58838a0d2db
im1gr4nt <januch1993@gmail.com>
2021-10-12 18:05:01 +0200

    unity digit

--- translator.py
+++ translator.py
@@ -1,12 +1,33 @@
+#!/usr/bin/python3
 
 
 
 
-
-num = input()
+num = input("Enter number: ")
 l = []
 
 for i in str(num):
     l.append(int(i))
 
+#unity digit
+if l[-1] == 1:
+    print("one")
+elif l[-1] == 2:
+    print("two")
+elif l[-1] == 3:
+    print("three")
+elif l[-1] == 4:
+    print("four")
+elif l[-1] == 5:
+    print("five")
+elif l[-1] == 6:
+    print("six")
+elif l[-1] == 7:
+    print("seven")
+elif l[-1] == 8:
+    print("eight")
+elif l[-1] == 9:
+    print("nine")
+else 
+    print("zero")
 print(l)
