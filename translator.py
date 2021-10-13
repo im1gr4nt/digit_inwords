ba2332aa64584d86f7f5afb8ffad302ce9be7486
Dawid <1amim1gr4nt@gmail.com>
2021-10-13 11:37:09 +0200

    added thousands

--- translator.py
+++ translator.py
@@ -18,33 +18,42 @@ for i in str(num):
 unit_l = ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'zero']
 teens_l = ['eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen']
 tens_l = ['ten', 'twenty', 'thirty', 'fourty', 'fivety', 'sixty', 'seventy', 'eighty', 'ninety']
-hund_l = 'hundred'
-hunds_l = 'hundreds'
-thou_l = 'thousand'
-thous_l = 'thousands'
+hunds = ' hundreds'
+thous = ' thousands'
 
 def units():
     n = l[-1]
     unit = unit_l[n-1]
     inwords.append(unit)
 
-def teens():
-    n = l[-2]
-    teen = teens_l[n-1]
-    inwords.append(teen)
-
 def tens():
-    n = l[-2]
-    ten = tens_l[n-1]
-    inwords.append(ten)
+    if l[-2] == 1 and l[-1] == 0:
+        n = l[-2]
+        teen = teens_l[n-1]
+        inwords.append(teen)
+    else:
+        n = l[-2]
+        ten = tens_l[n-1]
+        inwords.append(ten)
 
 def hundreds():
     if l[-3] == 1:
         inwords.append('one hundred')
-    n = l[-3]
-    hundred = hubd_l[n-1]
-    inwords.append(ten)
+    else:
+        n = l[-3]
+        unit = unit_l[n-1]
+        inwords.append(unit + hunds)
 
+def thousands():
+    if l[-4] == 1:
+        inwords.append('one thousand')
+    else:
+        n = l[-4]
+        unit = unit_l[n-1]
+        inwords.append(unit + thous)
+
+thousands()
+hundreds()
 if l[-2] == 1 and l[-1] != 0:
     teens()
 else:
