c30a75768620e73d947e42b2221eb16f39660d90
Dawid <1amim1gr4nt@gmail.com>
2021-10-13 11:08:13 +0200

    added functions for choosing numbers

--- translator.py
+++ translator.py
@@ -4,22 +4,51 @@
 
 
 num = input("Enter number: ")
+#list made of input digits
 l = []
+#list made of strings - number inwords - printed finally
+inwords = []
 
 #get a list of integers in number
 for i in str(num):
     l.append(int(i))
 
 
-#lists with numbers
-unit = ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'zero']
-teens = ['eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen']
-tens = ['ten', 'twenty', 'thirty', 'fourty', 'fivety', 'sixty', 'seventy', 'eighty', 'ninety']
-hund = 'hundred'
-hunds = 'hundreds'
-thou = 'thousand'
-thous = 'thousands'
+#lists with numbers in words
+unit_l = ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'zero']
+teens_l = ['eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen']
+tens_l = ['ten', 'twenty', 'thirty', 'fourty', 'fivety', 'sixty', 'seventy', 'eighty', 'ninety']
+hund_l = 'hundred'
+hunds_l = 'hundreds'
+thou_l = 'thousand'
+thous_l = 'thousands'
 
+def units():
+    n = l[-1]
+    unit = unit_l[n-1]
+    inwords.append(unit)
+
+def teens():
+    n = l[-2]
+    teen = teens_l[n-1]
+    inwords.append(teen)
+
+
+def tens():
+    n = l[-2]
+    ten = tens_l[n-1]
+    inwords.append(ten)
+
+if l[-2] == 1 and l[-1] != 0:
+    teens()
+else:
+    tens()
+
+units()
+
+print(*inwords)
+
+"""
 #number of tens
 if l[-2] == 1 and l[-1] == 1:
     tens = "eleven"
@@ -85,3 +114,4 @@ else:
 
 inwords = tens + " " + uni
 print(inwords)
+"""
