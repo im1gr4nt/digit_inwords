faa36bb3c9d6272f99d21b6da8645862c7fc53b9
Dawid <1amim1gr4nt@gmail.com>
2022-01-24 13:37:39 +0100

    works up to 99

--- inwords.py
+++ inwords.py
@@ -1,23 +1,32 @@
 #!/usr/bin/python3
 
 #list of numbers
-digits = ['zero', 'one', 'two','tree', 'four', 'five', 'six', 'seven', 'eight', 'nine']
-teens = ['zero', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eigteen', 'nineteen']
-tens = ['zero', 'ten', 'twenty', 'thirty', 'fourty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety']
+digits = ['zer', 'one', 'two','three', 'four', 'five', 'six', 'seven', 'eight', 'nine']
+teens = ['zer', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eigteen', 'nineteen']
+tens = ['zer', 'ten', 'twenty', 'thirty', 'fourty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety']
 
 #retrieve a number from user
 number = input("Insert your number: ")
 
 #create a list where separate digits from retrieved number will be stored
-leest = []
-#add single digits of which a provisioned number consists to a leest list 
+l = []
+#add single digits of which a provisioned number consists to a l list 
 for i in number:
-    leest.append(int(i))
+    l.append(int(i))
 
 #conditional based on number length
-if len(leest) == 1:
-    for i in leest:
-        print(digits[i])
-elif len(leest) == 2:
-
-
+if len(l) == 1:
+    print(digits[l[0]])
+elif len(l) == 2:
+    if l[0] == 1 and l[1]!=0:
+       print(teens[l[1]])
+    elif l[0]!=1 and l[1] == 0:
+        print(tens[l[0]])
+    elif l[0] == 1 and l[1] == 0:
+        print(tens[1])
+    elif l[0]!=1 and l[1]!=0:
+        print(tens[l[0]], digits[l[1]])
+    else:
+        pass
+else:
+    pass
