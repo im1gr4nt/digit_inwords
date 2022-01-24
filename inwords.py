bd24f96c43db2f0230b962c93905f58fca392e0e
Dawid <1amim1gr4nt@gmail.com>
2022-01-24 09:33:20 +0100

    small tidying

--- inwords.py
+++ inwords.py
@@ -1,13 +1,23 @@
 #!/usr/bin/python3
 
+#list of numbers
 digits = ['zero', 'one', 'two','tree', 'four', 'five', 'six', 'seven', 'eight', 'nine']
 teens = ['zero', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eigteen', 'nineteen']
-number = input("Insert your number: ")
-leest = []
+tens = ['zero', 'ten', 'twenty', 'thirty', 'fourty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety']
 
+#retrieve a number from user
+number = input("Insert your number: ")
+
+#create a list where separate digits from retrieved number will be stored
+leest = []
+#add single digits of which a provisioned number consists to a leest list 
 for i in number:
     leest.append(int(i))
 
+#conditional based on number length
+if len(leest) == 1:
+    for i in leest:
+        print(digits[i])
+elif len(leest) == 2:
 
-for i in leest:
-    print(digits[i])
+
